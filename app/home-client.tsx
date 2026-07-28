410a2f524427d96f95d0c44931134a4a2e0b156d
Yoonjae7 <jack123kor@gmail.com>
2026-07-28 16:40:04 +0900

    feat: replace kids BJJ logo with new updated version

--- app/home-client.tsx
+++ app/home-client.tsx
@@ -309,7 +309,7 @@ export default function Home() {
               <div className="flex flex-col items-center gap-2 text-center">
                 <div className="flex h-24 items-center justify-center sm:h-28">
                   <img
-                    src="/kids-logo.jpg"
+                    src="/kids-logo.png"
                     alt={t[language].kidsLogoAlt}
                     className="max-h-full w-auto max-w-[min(100vw-3rem,200px)] object-contain"
                   />
